6960d1514a069a4b2699a7ee6903fb50080e4e34
Shashank Dubey <55014190+shashankdubey78@users.noreply.github.com>
2023-05-06 03:31:32 -0400

    Update 734. Sentence Similarity.py
    Touple added in set

--- 734. Sentence Similarity.py
+++ 734. Sentence Similarity.py
@@ -1,6 +1,6 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
-
+#touple matched
         if len(sentence1)!=len(sentence2):
             return False
         
