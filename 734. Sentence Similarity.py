c7f879a6452fd0fe35ac0f2bc232b8ebe9287c53
Shashank Dubey <55014190+shashankdubey78@users.noreply.github.com>
2023-05-06 03:32:52 -0400

    Update 734. Sentence Similarity.py
    Both possible pairs added, sacrificed space for time.

--- 734. Sentence Similarity.py
+++ 734. Sentence Similarity.py
@@ -1,6 +1,6 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
-#touple matched
+
         if len(sentence1)!=len(sentence2):
             return False
         
@@ -8,10 +8,11 @@ class Solution:
             pairSet=set()
             for pair in similarPairs:
                 pairSet.add((pair[0],pair[1]))
+                pairSet.add((pair[1],pair[0]))
             for i in range(len(sentence1)):
                 if sentence1[i]==sentence2[i]:
                     continue
-                if not((sentence1[i],sentence2[i]) in pairSet or (sentence2[i],sentence1[i]) in pairSet):
+                if (sentence1[i],sentence2[i]) not in pairSet:
                     return False
 
             return True
